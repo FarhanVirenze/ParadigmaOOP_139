de379143ecc4178b0e21b1437a5de2542a25ed09
FarhanVirenze <farhanvirenze18@gmail.com>
2023-05-22 14:51:07 +0700

    tambahan fungsi void tambahDokter(dokter*)

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -6,7 +6,18 @@
 using namespace std;
 
 class dokter;
-
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(dokter*);
+};
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
