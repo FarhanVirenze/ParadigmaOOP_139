b4e37ba1f8fb32f0a62cd65e3e80bb0cb1f1ae4f
FarhanVirenze <farhanvirenze18@gmail.com>
2023-05-22 15:02:08 +0700

    tambahan fungsi void cetakPasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -36,9 +36,10 @@ public:
 	}
 
 	void tambahPasien(pasien*);
-
-
+	void cetakPasien();
 };
+
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
