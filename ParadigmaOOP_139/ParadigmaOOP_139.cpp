10d209fc9a28885a8ca64de25750fa93262eccf7
FarhanVirenze <farhanvirenze18@gmail.com>
2023-05-22 11:44:22 +0700

    tambahan fungsi return 0

--- ParadigmaOOP_139/ParadigmaOOP_139.cpp
+++ ParadigmaOOP_139/ParadigmaOOP_139.cpp
@@ -40,7 +40,11 @@ public:
 };
 
 int main() {
+	pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
 
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
