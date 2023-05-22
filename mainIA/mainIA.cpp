50e77076f68ee96fd506013db9f4e58c608b2dee
FarhanVirenze <farhanvirenze18@gmail.com>
2023-05-22 16:09:46 +0700

    tambahan fungsi return 0

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -9,8 +9,29 @@ using namespace std;
 #include "ibu.h"
 
 int main() {
+	ibu* varIbu = new ibu("dini");
+	ibu* varibu2 = new ibu("novi");
+	anak* varAnak1 = new anak("tono");
+	anak* varAnak2 = new anak("rini");
+	anak* varAnak3 = new anak("Dewi");
 
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varibu2->tambahAnak(varAnak3);
+	varibu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varibu2->cetakAnak();
+
+	delete varIbu;
+	delete varibu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+
+	return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
